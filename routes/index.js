5218bc0d98a3e4d760fd0fd7ef7cd90600afe328
jia_wonders <sylujia@163.com>
2016-09-01 17:19:34 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var user = require('../database/db').user;
+var UserModel = require('../database/db').UserModel;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -21,7 +21,7 @@ router.get('/login', function (req, res) {
 router.post('/ucenter', function (req, res) {
     var query = {name: req.body.name, password: req.body.password};
     (function () {
-        user.count(query, function (err, doc) {    //count返回集合中文档的数量，和 find 一样可以接收查询条件。query 表示查询的条件
+        UserModel.count(query, function (err, doc) {    //count返回集合中文档的数量，和 find 一样可以接收查询条件。query 表示查询的条件
             if (doc == 1) {
                 console.log(query.name + ": 登陆成功 " + new Date());
                 res.render('ucenter', {title: 'ucenter'});
